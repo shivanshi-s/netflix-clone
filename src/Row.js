6a59cd40d6271fc1b938406376d5775ac47f2805
shivanshi-s <shivanshi.saxena2@gmail.com>
2022-10-31 15:03:34 +0530

    Final touches

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from './axios';
 import './Row.css';
+import Youtube from 'react-youtube'
+import movieTrailer from "movie-trailer"
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow}) {
    const [movies, setMovies] = useState([]);
+   const [trailerUrl, setTrailerUrl] = useState("");
+
    // when row loads i want to load information for all
    useEffect( () => {
       async function fetchData() {
@@ -16,6 +20,26 @@ function Row({ title, fetchUrl, isLargeRow}) {
       }
       fetchData();
    }, [fetchUrl]);
+   
+   const opts = {
+      height : "390",
+      width : "100%",
+      playerVars: {
+         autoplay : 1,
+      },
+   };
+
+   const handleClick = (movie) => {
+      if(trailerUrl) {
+         setTrailerUrl('');
+      } else {
+         movieTrailer(movie?.name || "")
+         .then(url => {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get('v'));
+         }).catch((error) => console.log(error))
+      }
+   }
 
   return (
     <div className='row'>
@@ -26,10 +50,12 @@ function Row({ title, fetchUrl, isLargeRow}) {
             {movies.map(movie => (
                <img
                   key={movie.id}
+                  onClick={() => handleClick(movie)}
                   className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                   src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}/>
             ))}
          </div>
+         {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
     </div>
   )
 }
